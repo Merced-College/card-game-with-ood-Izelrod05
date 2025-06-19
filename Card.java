0e85ce86143dcd613c1d0a187911ba9aed6a0d8b
Izelrod05 <irodriguezdiaz@ucmerced.edu>
2025-06-19 06:09:31 +0000

    implemented getter and setter methods as well as print and comparison fnc

--- Card.java
+++ Card.java
@@ -1,9 +1,68 @@
+package cardGame;
+/*this is the card class which is in charg of all the getter and setter method 
+  as well as the printing method and comparing method
+ */
+
 public class Card {
+//private varibles 
+private String cardSuit;
+private String cardName;
+private int cardValue;
+private String cardPicture;
 
 
+//Constructor
 
+public Card(String cardSuit,String cardName,int cardValue,String cardPicture){
 
+        this.cardSuit = cardSuit;
+        this.cardName = cardName;
+        this.cardValue = cardValue;
+        this.cardPicture = cardPicture; 
 
-   
-    
+}
+
+//getter methods
+
+public String getCardSuit(){
+    return cardSuit;
+}
+
+public String getCardName(){
+    return cardName;
+}
+
+public int getCardValue(){
+    return cardValue;
+}
+public String getCardPicture(){
+    return cardPicture;
+}
+
+//setter methhods 
+public void setCardName(String cardName){
+    this.cardName = cardName;
+}
+public void setCardSuit(String cardSuit){
+    this.cardSuit = cardSuit;
+}
+public void setCardValue(int cardValue){
+    this.cardValue = cardValue;
+}
+public void setCardPicture(String cardPicture){
+    this.cardPicture = cardPicture;
+}
+//printing method
+public String toString(){
+    return cardName + " of " + cardSuit + " (Value: " + cardValue +")" ;
+}
+//comparing cards method
+public boolean equals(Object obj){
+    if (obj == null || !(obj instanceof Card)) return false;
+    Card other = (Card) obj;
+     return this.cardName.equals(other.cardName) &&
+        this.cardSuit.equals(other.cardSuit);
+
+}
+
 }
